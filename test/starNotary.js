b693ad8827e009d3d2d3574aeb6b566b6d1ce0c8
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-06-24 01:52:09 +0530

    Modified test

--- test/starNotary.js
+++ test/starNotary.js
@@ -24,21 +24,21 @@ it("lets user1 put star for sale",async()=>{
   assert.equal(await instance._tokenidtoPrice.call(starID),starCost)
 });
 
-it("lets user1 get the funds after sale",async()=>{
-  let instance=await starNotary.deployed();
-  let user1=accounts[1];
-  let user2=accounts[2];
-  let starID=3;
-  let starCost=web3.utils.toWei("0.01","ether");
-  let etherGiven=web3.utils.toWei("0.01","ether");
-  await instance.createStar("third star",starID,{from:user1});
-  await instance.starforSale(starID,starCost,{from:user1});
-  let balanceOfuser1before=await web3.eth.getBalance(user1);
-  await instance.buyStar(starID,{from:user2,value:etherGiven,gasPrice:0});
-  let balanceOfuser1after=web3.eth.getBalance(user1);
-  let value1=Number(balanceOfuser1before)+Number(starCost);
-  let value2=Number(balanceOfuser1after);
-  assert.equal(isNaN(value2),isNaN(value1));
+it('lets user1 get the funds after the sale', async() => {
+    let instance = await StarNotary.deployed();
+    let user1 = accounts[1];
+    let user2 = accounts[2];
+    let starId = 3;
+    let starPrice = web3.utils.toWei(".01", "ether");
+    let balance = web3.utils.toWei(".05", "ether");
+    await instance.createStar('awesome star', starId, {from: user1});
+    await instance.putStarUpForSale(starId, starPrice, {from: user1});
+    let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user1);
+    await instance.buyStar(starId, {from: user2, value: balance});
+    let balanceOfUser1AfterTransaction = await web3.eth.getBalance(user1);
+    let value1 = Number(balanceOfUser1BeforeTransaction) + Number(starPrice);
+    let value2 = Number(balanceOfUser1AfterTransaction);
+    assert.equal(value1, value2);
 });
 
 it("lets user2 buy a star, if it is put up for sale",async()=>{
